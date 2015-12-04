cbfc17e7018b8be058e3cd7d50fa75916dc95a4a
NHS1991 <hoangson1991@gmail.com>
2015-12-04 15:50:09 +0100

    Add comments

--- src/detection_visage.py
+++ src/detection_visage.py
@@ -18,48 +18,57 @@ class DetecteurVisage:
         self.THRESHOLD = 0.75
 
     def execute(self,images_test_dir):
+        # Delete all images in the result folder
         for img_file in os.listdir(src_dir+"/result/"):
             file_path = os.path.join(src_dir+"/result/", img_file)
             try:
                 if os.path.splitext(img_file)[1][1:] == "pgm" or os.path.splitext(img_file)[1][1:] == "jpg" or os.path.splitext(img_file)[1][1:] == "jpeg" or os.path.splitext(img_file)[1][1:] == "png":
                     os.unlink(file_path)
-                #elif os.path.isdir(file_path): shutil.rmtree(file_path)
             except Exception, e:
                 print e
         cv2.namedWindow('detection_visage', cv2.WINDOW_AUTOSIZE)
         for img_file in os.listdir(images_test_dir):
+            # Check image files in test folder
             if os.path.splitext(img_file)[1][1:] == "pgm" or os.path.splitext(img_file)[1][1:] == "jpg" or os.path.splitext(img_file)[1][1:] == "jpeg" or os.path.splitext(img_file)[1][1:] == "png":
                 print "Fichier: "+ img_file
                 img_file_path = images_test_dir+"/"+img_file
+                # Read the input image
                 img_cv = cv2.imread(img_file_path)
                 img_height, img_width = img_cv.shape[:2]
                 found = False
                 count = 0
                 all_boxes = []
-                img_cv_copy = img_cv.copy()
-                for py_img in pyramid(img_cv_copy,scale = self.SCALE_FACTOR):
+                # Convert BGR image to gray
+                img_cv_gray = cv2.cvtColor(img_cv,cv2.COLOR_BGR2GRAY)
+                for py_img in pyramid(img_cv_gray,scale = self.SCALE_FACTOR):
                     scale = pow(self.SCALE_FACTOR,count)
                     count += 1
                     #loop over the sliding window for each layer of the pyramid
                     for (x, y, window) in sliding_window(py_img, self.STRIDE, self.WINDOW_SIZE):
-                        window_input = window[np.newaxis,np.newaxis,:,:]
+                        # Apply the CNN to position (x,y)
+                        window_input = window[np.newaxis,np.newaxis,:]
                         self.net.blobs['data'].reshape(*window_input.shape)
                         self.net.blobs['data'].data[...] = window_input
                         out = self.net.forward()
                         # get output value of the layer 'prob'
                         prob_val = out['prob'][0][1]
+                        # Check output value is greater than the given threshold (a face appears probably at this
+                        # position (x,y))
                         if prob_val >= self.THRESHOLD:
-                            box = generateBoundingBox(x, y, prob_val, self.WINDOW_SIZE, scale)
+                            box = generateBoundingBox(x, y, prob_val, self.WINDOW_SIZE, scale,img_width,img_height)
                             all_boxes.append(box)
                 if all_boxes:
                     found = True
-                    all_boxes = removeNeighborsBox(all_boxes,img_width,img_height)
+                    all_boxes = removeNeighborsBox(all_boxes)
+                #draw all rectangles at faces positions
                 for box in all_boxes:
                     cv2.rectangle(img_cv,(box[0], box[1]),(box[2],box[3]),(0,0,255))
                     cv2.imshow('detection_visage',img_cv)
                 if found == True:
+                    #Pause the program to see the result and press the enter key to continue the program
                     raw_input("Presser 'enter' pour continuer...")
                     cv2.imwrite(src_dir+"/result/"+img_file,img_cv)
+        # Pause the program to see the last result and press the enter key to terminate the program
         raw_input("Presser 'enter' pour terminer...")
         cv2.destroyAllWindows()
 
@@ -71,38 +80,35 @@ def sliding_window(image, stepSize, windowSize):
             yield (x, y, image[y:y + windowSize[1], x:x + windowSize[0]])
 
 def pyramid(image, scale, minSize=(70, 70)):
-    # yield the original image
-    yield toGray(np.array(image))
+    # return the original image
+    yield np.array(image)
     # keep looping over the pyramid
     while True:
         # resize the image
-        height, width = image.shape[:2]
-        image = cv2.resize(image,(int(width/scale), int(height/scale)))
+        new_width = int(image.shape[1]/scale)
+        new_height = int(image.shape[0]/scale)
+        image = cv2.resize(image,(new_width, new_height))
         # if the resized image does not meet the supplied minimum
         # size, then stop constructing the pyramid
-        if image.shape[0] < minSize[1] or image.shape[1] < minSize[0]:
+        if new_width < minSize[0] or new_height < minSize[1]:
             break
-        # yield the next image in the pyramid
-        yield toGray(np.array(image))
+        # return the next image in the pyramid
+        yield np.array(image)
 
 # Rectangle
-def generateBoundingBox(x,y,prob_val,window_size, scale):
+def generateBoundingBox(x,y,prob_val,window_size, scale,img_width,img_height):
     real_x = int(x * scale)
     real_y = int(y * scale)
     real_w = int(window_size[0] * scale)
     real_h = int(window_size[1] * scale)
     print 'Visage détecté à Rect('+str(real_x)+','+str(real_y)+','+str(real_x+real_w)+','+str(real_y+real_h)+') prob='+str(prob_val)
-    return [real_x, real_y, real_x+real_w, real_y+real_h]
+    return [real_x, real_y, min(real_x+real_w,img_width), min(real_y+real_h,img_height)]
 
-# convert color image to gray image
-def toGray(bgr):
-    b, g, r = bgr[:, :, 0], bgr[:, :, 1], bgr[:, :, 2]
-    gray = 0.299 * r + 0.587 * g + 0.114 * b
-    return gray
 # Remove neighbors rectangle
-def removeNeighborsBox(boxes,img_width,img_height):
+def removeNeighborsBox(boxes):
     len_boxes = len(boxes)
     i = 0
+    #sort the rectangles list by its coordinates vertex points
     boxes = sortedlist(boxes,key=lambda x:(x[0],x[1],x[2],x[3]))
     while i < len_boxes-1:
         box1 = boxes[i]
@@ -126,7 +132,8 @@ def removeNeighborsBox(boxes,img_width,img_height):
         j = i+1
         while j < len_boxes:
             box2 = boxes[j]
-            if (box2[0]-box1[0] < img_width/20.0 and box2[1]==box1[1]) or (box2[1]-box1[1] < img_height/20.0 and box2[0]==box1[0]):
+            # If the distances between 2 vertex points same x-coordinate or y-coordinate of 2 rectangles is lower than 10
+            if (box2[0]-box1[0] < 10 and box2[1]==box1[1]) or (box2[1]-box1[1] < 10 and box2[0]==box1[0]):
                 del boxes[i]
                 i -= 1
                 del boxes[j-1]
@@ -136,7 +143,6 @@ def removeNeighborsBox(boxes,img_width,img_height):
             j += 1
         i += 1
     return boxes
-
 
 #Find max, min between 2 values
 def min(x,y):
